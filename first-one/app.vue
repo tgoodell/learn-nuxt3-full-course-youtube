c8ade7cf5b57a540c84f8a3c4442052da43a843e
Tristan Goodell <>
2023-05-24 10:59:08 -0400

    End first section, moving onto Components.

--- first-one/app.vue
+++ first-one/app.vue
@@ -1,6 +1,16 @@
 <template>
   <div>
     <NuxtLayout>
+      <header>
+        <ul>
+          <li>
+            <NuxtLink to="/">Home</NuxtLink>
+          </li>
+          <li>
+            <NuxtLink to="/events">Events</NuxtLink>
+          </li>
+        </ul>
+      </header>
       <NuxtPage />
     </NuxtLayout>
   </div>
--- first-one/pages/events/[id].vue
+++ first-one/pages/events/[id].vue
@@ -1,7 +1,7 @@
 <script setup>
-const route = useRoute();
+const nuxt = useNuxtApp();
 
-console.log(route.params.id);
+console.log(nuxt);
 </script>
 
 <template>

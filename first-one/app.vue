8e4fae5750934e282f327fddd3f044f16ef06468
Tristan Goodell <>
2023-05-24 10:43:21 -0400

    Added sass, TailwindCSS

--- first-one/app.vue
+++ first-one/app.vue
@@ -1,5 +1,5 @@
 <template>
   <div>
-    <NuxtWelcome />
+    Hello Nuxt3!
   </div>
 </template>
--- first-one/nuxt.config.ts
+++ first-one/nuxt.config.ts
@@ -1,4 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import { resolve } from "path";
 export default defineNuxtConfig({
-
+    alias: {
+        '@': resolve(__dirname, "/"),
+    },
+    css: [
+        "~/assets/main.scss",
+    ],
+    postcss: {
+        plugins: {
+          tailwindcss: {},
+          autoprefixer: {},
+        },
+    },
 })
